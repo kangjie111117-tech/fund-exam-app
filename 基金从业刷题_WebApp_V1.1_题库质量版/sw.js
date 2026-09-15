ad7bfff2f55a3d18d8c605d3196e383e92f48307
kangjie111117-tech <kangjie111117@gmail.com>
2026-09-15 16:30:36 +0800

    Delete 基金从业刷题_WebApp_V1.1_题库质量版 directory

--- 基金从业刷题_WebApp_V1.1_题库质量版/sw.js
+++
@@ -1,1 +0,0 @@
-const C='fund-v11';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./questions.js','./manifest.webmanifest','./icon-192.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
