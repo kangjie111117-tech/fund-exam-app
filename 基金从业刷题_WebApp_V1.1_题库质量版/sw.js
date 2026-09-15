03928c86706d727b1669594a735a27d241157ccf
kangjie111117-tech <kangjie111117@gmail.com>
2026-09-15 15:53:37 +0800

    Add files via upload

---
+++ 基金从业刷题_WebApp_V1.1_题库质量版/sw.js
@@ -0,0 +1,1 @@
+const C='fund-v11';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./questions.js','./manifest.webmanifest','./icon-192.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
